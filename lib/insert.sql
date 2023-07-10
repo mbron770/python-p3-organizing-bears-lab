011c0d49fefe01a5cba3df1a13a8c0a456af0ca5
mbron770 <mbronfin.tsd@gmail.com>
2023-07-09 20:03:50 -0400

    a

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 25, 'M', 'Orange', 'Mad', 
+'1'); 
+INSERT INTO bears VALUES (2, 'Rowdy', 34, 'F', 'Green', 'angry',
+'0');
+INSERT INTO bears VALUES (3, 'Tabita', 27, 'M', 'Yellow', 
+'Mad', '1');
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 78, 'M', 'Grey', 
+'Grumpy', '1');
+INSERT INTO bears VALUES (5, 'Melissa', 90, 'M', 'Orange', 
+'Mad', '1');
+INSERT INTO bears VALUES (6, 'Grinch', 89, 'M', 'Orange', 'Mad',
+'1');
+INSERT INTO bears VALUES (7, 'Wendy', 300, 'F', 'Orange', 'Mad',
+'1');
+INSERT INTO bears VALUES (8, NULL, 89, 'M', 'Orange', 'Mad',
+'1');
